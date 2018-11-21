d165a3b845e4c821336ae4e6d114f434e46e1403
zuowenjie <zuowenjie@sunlands.com>
2018-11-21 10:34:33 +0800

    基于注解的mybatis多数据源配置，注意jdbc-url参数配置 启动类中注意删除@MapperScan注解

--- springboot2.0-yipinketang-parent/springboot2.0-yipinketang-mybatis-annotation-multiDataSource/src/main/java/com/yipinketang/app/AppYiPinKeTangMybatisAnnotationMultiDataSource.java
+++ springboot2.0-yipinketang-parent/springboot2.0-yipinketang-mybatis-annotation-multiDataSource/src/main/java/com/yipinketang/app/AppYiPinKeTangMybatisAnnotationMultiDataSource.java
@@ -1,6 +1,5 @@
 package com.yipinketang.app;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
-@MapperScan("com.yipinketang.app.mapper")
+//注意：因为已经有DataSource的Configuration配置文件，已经添加了@MapperScan注解，所以这个地方再添加是会报错的：No MyBatis mapper was found in '[com
+// .yipinketang.app.mapper]' package. Please check your configuration.
+//@MapperScan("com.yipinketang.app.mapper")
 public class AppYiPinKeTangMybatisAnnotationMultiDataSource {
 
     public static void main(String[] args){
